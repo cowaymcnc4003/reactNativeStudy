dc7c5d06cc9eeeed83b3800e9323de833e240c73
mhlee <mcnccoway4003@gmail.com>
2024-11-25 12:57:18 +0900

    1.home icon ui

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,40 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { StatusBar, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{ backgroundColor: 'white' }}>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+      <View style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 15,
+      }}>
+        <View>
+          <Text
+            style={{
+              fontSize: 25,
+              fontWeight: 500,
+              color: 'black',
+            }}
+          >mhleeApp</Text>
+        </View>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}
+        >
+          <FontAwesome name="plus-square-o" style={{ fontSize: 24, paddingHorizontal: 15 }} />
+          <Feather name="navigation" style={{ fontSize: 24 }} />
+        </View>
+      </View>
+    </SafeAreaView >
   );
 };
 
